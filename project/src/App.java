1c6235110f81cd836e861157b31648a317e72bf2
vitordcgomes <vitordcgomes@gmail.com>
2023-10-22 22:52:32 -0300

    build add/update and compile/run tests - 'App.java' working!

--- project/src/App.java
+++ project/src/App.java
@@ -1,5 +1,34 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        
+        if (args.length != 4) {
+            System.out.println("Correct use: java -jar deputados.jar <opção_de_cargo> <caminho_arquivo_candidatos> <caminho_arquivo_votacao> <data>");
+            return;
+        }
+        
+        String cargo = args[0];
+        String arquivoCandidatos = args[1];
+        String arquivoVotacao = args[2];
+        String dataEleicao = args[3];
+
+        // Validate date format
+        if (!Pattern.matches("\\d{2}/\\d{2}/\\d{4}", dataEleicao)) {
+            System.out.println("Invalid Date Format. Use dd/MM/yyyy");
+            return;
+        }
+
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Arquivo de Candidatos: " + arquivoCandidatos);
+        System.out.println("Arquivo de Votação: " + arquivoVotacao);
+        System.out.println("Data da Eleição: " + dataEleicao);
+
+        CSVReader reader = new CSVReader();
+        reader.readerCandidates(arquivoCandidatos);
+
     }
 }
--- project/src/CSVReader.java
+++ project/src/CSVReader.java
@@ -1,6 +1,4 @@
 import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 // how to compile/run (from 'src' directory):
@@ -8,66 +6,22 @@ import java.util.Scanner;
 // run: java -cp ../bin CSVReader
 
 public class CSVReader {
-    public static void main(String[] args) {
-        /*
-        if (args.length != 4) {
-            System.out.println("Uso correto: java -jar deputados.jar <opção_de_cargo> <caminho_arquivo_candidatos> <caminho_arquivo_votacao> <data>");
-            return;
-        }
-        */
+    private String candidatesFile;
+    private String votesFile;
 
-        String tipo = args[0];
-        String arquivoCandidatos = args[1];
-        String arquivoVotacao = args[2];
-        String dataEleicaoString = args[3];
+    public void readerCandidates(String candidatesFile) {
+        try (FileInputStream fin = new FileInputStream(candidatesFile);
+                Scanner s = new Scanner(fin, "ISO-8859-1")) {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date dataEleicao;
-        
-        try {
-            dataEleicao = dateFormat.parse(dataEleicaoString);
-        } catch (Exception e) {
-            System.out.println("Formato de data inválido. Use dd/MM/yyyy");
-            return;
-        }
-
-        System.out.println("Tipo: " + tipo);
-        System.out.println("Arquivo de Candidatos: " + arquivoCandidatos);
-        System.out.println("Arquivo de Votação: " + arquivoVotacao);
-        System.out.println("Data da Eleição: " + dataEleicao);
-
-        try (FileInputStream finCandidatos = new FileInputStream(arquivoCandidatos);
-             Scanner sCandidatos = new Scanner(finCandidatos, "ISO-8859-1");
-             FileInputStream finVotacao = new FileInputStream(arquivoVotacao);
-             Scanner sVotacao = new Scanner(finVotacao, "ISO-8859-1")) {
-
-            // Processar arquivo de consulta de candidatos
-            while (sCandidatos.hasNextLine()) {
-                String line = sCandidatos.nextLine();
-                System.out.println("Processando linha de candidato: " + line);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                System.out.println("Processando linha: " + line);
                 Scanner lineScanner = new Scanner(line);
                 lineScanner.useDelimiter(";");
 
                 while(lineScanner.hasNext()) {
                     String token = lineScanner.next();
-                    System.out.println("Leu candidato: [" + token + "]");
-                    // Faça o processamento dos dados do candidato aqui
-                }
-
-                lineScanner.close();
-            }
-
-            // Processar arquivo de seção de votação
-            while (sVotacao.hasNextLine()) {
-                String line = sVotacao.nextLine();
-                System.out.println("Processando linha de votação: " + line);
-                Scanner lineScanner = new Scanner(line);
-                lineScanner.useDelimiter(";");
-
-                while(lineScanner.hasNext()) {
-                    String token = lineScanner.next();
-                    System.out.println("Leu votação: [" + token + "]");
-                    // Faça o processamento dos dados de votação aqui
+                    System.out.println("Leu: [" + token + "]");
                 }
 
                 lineScanner.close();
@@ -75,5 +29,10 @@ public class CSVReader {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    
+    }
+
+    public void readerVotes(String votesFile) {
+        return;
     }
 }
